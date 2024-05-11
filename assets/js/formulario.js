be7ffd3e4feb5f450f1031bf5250cdbbfcab6914
prates <guipratesdsantos@gmail.com>
2024-05-11 16:47:52 -0300

    tratamento formulario

---
+++ assets/js/formulario.js
@@ -0,0 +1,70 @@
+class formulario {
+    constructor() {
+        this.form = document.querySelector('#form');
+        this.evento();
+    }
+
+    evento() {
+        this.form.addEventListener('submit', e => {
+            handleSubmit(e);
+        });
+    }
+
+    //Condição para envio do formulário
+    handleSubmit(e) {   
+        e.preventDefault();
+        const camposValidos = this.camposValidos();
+        if (camposValidos === true) {
+            alert('Formulário enviado!');
+            this.formulario.submit();
+        }
+    }
+
+    //Validação de todos os campos do formulário
+    camposValidos() {
+        let valid = true;
+
+        for(const errorText of this.form.querySelectorAll('.errorText')) {
+            errorText.remove();
+        }
+
+        //Validação geral dos campos
+        for (const campo of document.querySelectorAll('.validar')) {
+            if (!campo.value) {
+                this.criaError(campo, 'O campo não pode estar vazio.');
+                valid = false;
+            }
+            //Validação do nome do cartao
+            if (campo.id === 'cardholher') {
+                if (!this.validarNome(campo)) valid = false;
+            }
+            //Validação do numero do cartao
+            if (campo.id === 'num') {
+                if (!this.validarNumero(campo)) valid = false;
+            }
+            //Validação do mês
+            if (campo.id === 'date-mounth') {
+                if (!this.validarMes(campo)) valid = false;
+            }
+            //Validação do ano
+            if (campo.id === 'date-year') {
+                if (!this.validarAno(campo)) valid = false;
+            }
+            //Validação do numero CVC
+            if (campo.id === 'cvc') {
+                if (!this.validarCvc(campo)) valid = false;
+            }
+        }
+
+        return valid;
+    }
+
+    criaError(campo, errorText) {
+        const div = document.createElement('div');
+        div.innerHTML = errorText;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
+    }
+}
+
+const validandoFormulario = new formulario();
