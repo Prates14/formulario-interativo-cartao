a26ca608811149df0b79c8db019c85b8e63c326d
prates <guipratesdsantos@gmail.com>
2024-05-13 12:13:49 -0300

    tratamento formulario

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -16,7 +16,7 @@ class formulario {
         const camposValidos = this.camposValidos();
         if (camposValidos === true) {
             alert('Formulário enviado!');
-            this.formulario.submit();
+            this.form.submit();
         }
     }
 
@@ -31,36 +31,36 @@ class formulario {
         //Validação geral dos campos
         for (const campo of document.querySelectorAll('.validar')) {
             if (!campo.value) {
-                this.criaError(campo, 'O campo não pode estar vazio.');
+                this.criaError(campo, "Can't be blank.");
                 valid = false;
             }
             //Validação do nome do cartao
-            if (campo.id === 'cardholher') {
-                if (!this.validarNome(campo)) valid = false;
-            }
-            //Validação do numero do cartao
-            if (campo.id === 'num') {
-                if (!this.validarNumero(campo)) valid = false;
-            }
-            //Validação do mês
-            if (campo.id === 'date-mounth') {
-                if (!this.validarMes(campo)) valid = false;
-            }
-            //Validação do ano
-            if (campo.id === 'date-year') {
-                if (!this.validarAno(campo)) valid = false;
-            }
-            //Validação do numero CVC
-            if (campo.id === 'cvc') {
-                if (!this.validarCvc(campo)) valid = false;
-            }
+            // if (campo.id === 'cardholher') {
+            //     if (!this.validarNome(campo)) valid = false;
+            // }
+            // //Validação do numero do cartao
+            // if (campo.id === 'num') {
+            //     if (!this.validarNumero(campo)) valid = false;
+            // }
+            // //Validação do mês
+            // if (campo.id === 'date-mounth') {
+            //     if (!this.validarMes(campo)) valid = false;
+            // }
+            // //Validação do ano
+            // if (campo.id === 'date-year') {
+            //     if (!this.validarAno(campo)) valid = false;
+            // }
+            // //Validação do numero CVC
+            // if (campo.id === 'cvc') {
+            //     if (!this.validarCvc(campo)) valid = false;
+            // }
         }
 
         return valid;
     }
 
     criaError(campo, errorText) {
-        const div = document.createElement('div');
+        const div = document.createElement('p');
         div.innerHTML = errorText;
         div.classList.add('error-text');
         campo.insertAdjacentElement('afterend', div);
